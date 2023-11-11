9109d04965300bff18e2a73eb3c4550b773716ed
fani rafiul maarif <fanirafiul99@gmail.com>
2023-11-11 13:20:58 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle //
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klick di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
